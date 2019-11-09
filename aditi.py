5febeb9e892e01fabcebc6c0827688119fbc3433
Aditi jain <43278204+microaditi@users.noreply.github.com>
2019-11-09 17:49:45 +0530

    Add files via upload
    Dare 1 
    Dare completed ;-)

---
+++ aditi.py
@@ -0,0 +1,10 @@
+class aditi:
+
+    def __init__(self,num,name):
+        self.num = num
+        self.name = name
+
+    def display(self):
+        print("Aditi is " + self.num +" times better than " + self.name)
+
+
---
+++ import_aditi.py
@@ -0,0 +1,8 @@
+import aditi
+
+for i in range(0,5):
+    num = input("Enter number")
+    name = input("enter name")
+
+    person = aditi.aditi(num,name)
+    person.display()
